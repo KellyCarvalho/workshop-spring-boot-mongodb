6de4318bf103d820ad10039d4b756231120ba01e
KellyCarvalho <18kellycarvalho@gmail.com>
2021-01-13 19:22:30 -0300

    Iniciando aplicação

---
+++ src/main/java/com/springboot/custsew/CustsewApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.custsew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustsewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustsewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/custsew/CustsewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.custsew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustsewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
